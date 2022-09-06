9f1ff1fddd313eca5ea902744338dd7585977c0e
Nikita Dudnik <nikdudnik@gmail.com>
2022-09-06 15:45:15 +0200

    Fix emit to use buffer

--- src/faustLoader.ts
+++ src/faustLoader.ts
@@ -61,7 +61,7 @@ const faustLoader: LoaderDefinitionFunction<Options> = async function (
   const wasmContent = await fs.readFile(wasmPath);
   // TODO: this method should accept a buffer
   // PR: https://github.com/webpack/webpack/pull/13577
-  this.emitFile(path.join(outputPath, wasmName), wasmContent.toString());
+  this.emitFile(path.join(outputPath, wasmName), wasmContent);
 
   const processorName = interpolateName(this, "[name]-processor.js", {
     context,
